829084f938498c7a6d55704655ba1d1ef27c11bb
Daryn Smith <d.maccoy.smith@gmail.com>
2023-07-11 22:36:41 -0600

    add function to determine color of ray, add camera and viewport

--- main.cpp
+++ main.cpp
@@ -1,20 +1,47 @@
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
 
 #include <iostream>
 
+color ray_color(const ray r) {
+  // map y value of unit vector of given ray's direction between 0 and 1
+  vec3 unit_direction = unit_vector(r.direction());
+  double t = 0.5 * (unit_direction.y() + 1);
+  
+  color gradient_start(1, 1, 1);    // white
+  color gradient_end(0.5, 0.7, 1);  // sky blue
+  
+  // simple linear interpolation
+  return ((1 - t) * gradient_start) + (t * gradient_end);
+}
+
 int main() {
+  
   // image
-  const int image_width = 256;
-  const int image_height = 256;
+  const double aspect_ratio = 16.0 / 9.0;
+  const int image_width = 400;
+  const int image_height = static_cast<int>(image_width / aspect_ratio);
+  
+  // camera
+  const double viewport_height = 2.0;
+  const double viewport_width = aspect_ratio * viewport_height;
+  const double focal_length = 1.0;
+  const point3 origin(0, 0, 0);
+  const vec3 horizontal(viewport_width, 0, 0);
+  const vec3 vertical(0, viewport_height, 0);
+  const point3 lower_left_corner = origin - (horizontal / 2) - (vertical / 2) - vec3(0, 0, focal_length);
   
   // render
   std::cout << "P3" << std::endl << image_width << ' ' << image_height << std::endl << "255" << std::endl;
   for (int row = image_height - 1; row >= 0; row--) {
-    int row_count = image_height - row;
-    std::cerr << "\rProgress: " << row_count << '/' << image_height;
-    for (int pixel = 0; pixel < image_width; pixel++) {
-      color pixel_color(double(pixel) / (image_width - 1), double(row) / (image_height - 1), 0.25);
+    std::cerr << "\rProgress: " << image_height - row << '/' << image_height << std::flush;
+    for (int col = 0; col < image_width; col++) {
+      const double u = static_cast<double>(col) / (image_width - 1);
+      const double v = static_cast<double>(row) / (image_height - 1);
+      const vec3 direction = lower_left_corner + (u * horizontal) + (v * vertical) - origin;
+      const ray r(origin, direction);
+      color pixel_color = ray_color(r);
       write_color(std::cout, pixel_color);
     }
   }
